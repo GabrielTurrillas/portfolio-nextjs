04b4c39c75fc264042777d3fc3a69104026d9bf1
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-10-04 14:12:40 -0300

    Head included in _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Gabriel Turrillas - Desarrollador Web</title>
+        <meta name='description' content='Portafolios de Gabriel Turrillas. Desarrollador web. Portfolio of Gabriel Turrillas Web Developer' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+
 }
 
 export default MyApp
